764a962971bfcd8ddd47c005a1b303ca78a3beea
dcortes <dcortes@campus.udes.edu.co>
2019-02-18 18:59:54 -0500

    Added propTypes validation for all components

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Location = (props) => (
     <div>
@@ -7,5 +8,9 @@ const Location = (props) => (
         </h1>
     </div>
 );
+
+Location.propTypes = {
+    city: PropTypes.string.isRequired,
+};
 
 export default Location;
